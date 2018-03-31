22ed4d99d4f81a03d963fcc85fb1a244fba78006
LL <cpyfluoxiaohui@163.com>
2018-03-31 15:36:32 +0800

    开启csrf防护

--- manage.py
+++ manage.py
@@ -1,11 +1,15 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 import redis
 class Config(object):
     """加载配置"""
     # 开启调试模式
     DEBUG = True
+
+    # 秘钥
+    SECRET_KEY = '8hV4rv0opNaPoWc/lbCTsLc70IXTgRD3R+AtQMyvtwzuoFGk0TiWEg7cPwNoV1mp'
     # 配置mysql数据库:开发中使用真实IP
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/iHome'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -23,8 +27,10 @@ db = SQLAlchemy(app)
 # 创建redis链接对象
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 
+# 开启csrf防护
+CSRFProtect(app)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
     # 测试redis链接
     redis_store.set('name2','SS')
@@ -32,4 +38,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='192.168.241.154')
