5547ce74b2da6b5c1521511832eb096db9e3eca1
LL <cpyfluoxiaohui@163.com>
2018-03-31 15:22:06 +0800

    加载配置项

--- manage.py
+++ manage.py
@@ -1,7 +1,16 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
 
+class Config(object):
+    """加载配置"""
+    # 开启调试模式
+    DEBUG = True
+
 app = Flask(__name__)
+
+# 加载配置项
+app.config.from_object(Config)
+
 
 
 @app.route('/')
