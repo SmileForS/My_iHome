678e76c1d3c3f80db1ca4e7065b039e98e73647d
LL <cpyfluoxiaohui@163.com>
2018-04-01 20:21:55 +0800

    图片验证的后端逻辑实现和删除上次的验证码记录

--- iHome_LL/api_1_0/verify.py
+++ iHome_LL/api_1_0/verify.py
@@ -17,8 +17,26 @@ def get_image_code():
     # 3.使用uuid存储图片验证码内容
     # 4.返回图片验证码
 
-    # 生成图片验证码,text是验证码的文字信息，image验证码的图片信息
-    name,text,image= captcha.generate_captcha()
+    #1.获取uuid
+    uuid = request.args.get('uuid')
+    last_uuid = request.args.get('last_uuid')
+    if not uuid:
+        abort(403)
+        # return jsonify(errno=RET.PARAMERR,errmsg=u'缺少参数')
 
-    # 返回图片验证码
-    return image
+    #2. 生成图片验证码,text是验证码的文字信息，image验证码的图片信息
+    name,text,image= captcha.generate_captcha()
+    # 3.使用uuid存储图片验证码内容
+    try:
+        if last_uuid:
+            # 上次的uuid还在，需要在redis中删除
+            redis_store.delete('ImageCode:'+last_uuid)
+        # 保存本次需要记录的验证码数据
+        redis_store.set('ImageCode:'+uuid,text,constants.IMAGE_CODE_REDIS_EXPIRES)
+    except Exception as e:
+        print e
+        return jsonify(errno=RET.DBERR,errmsg=u'保存验证码数据失败')
+    #4 返回图片验证码
+    response = make_response(image)
+    response.headers['Content-Type']='image/jpg'
+    return response
