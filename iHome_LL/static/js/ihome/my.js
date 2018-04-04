f62e8393dd505e5093507c1c668e6fd9770953f9
LL <cpyfluoxiaohui@163.com>
2018-04-05 00:03:37 +0800

    用户个人中心页面数据展示

--- iHome_LL/static/js/ihome/my.js
+++ iHome_LL/static/js/ihome/my.js
@@ -11,5 +11,12 @@ function logout() {
 $(document).ready(function(){
 
     // TODO: 在页面加载完毕之后去加载个人信息
-
+    $.get('/api/1.0/users',function (response) {
+        if (response.errno =='0'){
+            $('#user-name').html(response.data.name);
+            $('#user-mobile').html(response.data.mobile);
+        }else {
+            alert(response.errmsg);
+        }
+    })
 });
