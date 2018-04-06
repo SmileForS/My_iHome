e089c14acd1da84513909071d5ca92ef53c17ab2
LL <cpyfluoxiaohui@163.com>
2018-04-06 17:06:55 +0800

    发布房源接口之前端逻辑实现

--- iHome_LL/static/js/ihome/newhouse.js
+++ iHome_LL/static/js/ihome/newhouse.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
        if (response.errno =='0'){
            $.each(response.data,function (i,area) {
                $('#area-id').append("<option value='"+area.aid+"'>"+area.aname+"</option>")
-               console.log(response.data)
+               // console.log(response.data)
            })
        }else {
            alert(response.errmsg)
@@ -21,7 +21,45 @@ $(document).ready(function(){
     });
 
     // TODO: 处理房屋基本信息提交的表单数据
-
+    $('#form-house-info').submit(function (event) {
+        event.preventDefault();
+        //准备参数
+        var params ={};
+        // var title = $('#house-title).val();
+        // params['title'] = title
+        //收集form表单中需要提交的input标签，放在一个数组对象中
+        //map遍历对象，比如说数组对象
+        // obj == {name:'title',value:'1'}
+        $(this).serializeArray().map(function (obj) {
+            // console.log(obj);
+            params[obj.name] = obj.value;
+        });
+        //收集房屋设施信息
+        // $(':checkbox:checked[name=facility]') == 收集界面上被选中的name为facility的checkbox
+        // each 遍历界面上收集到的标签
+        // elem ==<input type='checkbox' name='facility' value='2'>热水淋雨
+        facilities = [];
+        $(':checkbox:checked[name=facility]').each(function (i,elem) {
+            facilities[i] = elem.value;
+        });
+        params['facility'] = facilities;
+        $.ajax({
+           url:'/api/1.0/houses',
+           type:'post',
+           data:JSON.stringify(params),
+           contentType:'application/json',
+           headers:{'X-CSRFToken':getCookie('csrf_token')},
+           success:function (response) {
+               if (response.errno =='0'){
+                   alert(response.errmsg)
+               }else if(response.errno =='4101'){
+                   location.href='/'
+               }else{
+                   alert(response.errmsg);
+               }
+           }
+       })
+    });
     // TODO: 处理图片表单的数据
 
 });
