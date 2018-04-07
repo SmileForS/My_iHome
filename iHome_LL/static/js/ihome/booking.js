53e2d08cc5312be1a0da4667a62d61b0cd70f7f5
LL <cpyfluoxiaohui@163.com>
2018-04-07 21:38:15 +0800

    订单之预定界面展示

--- iHome_LL/static/js/ihome/booking.js
+++ iHome_LL/static/js/ihome/booking.js
@@ -26,6 +26,11 @@ function showErrorMsg() {
 
 $(document).ready(function(){
     // TODO: 判断用户是否登录
+    $.get('/api/1.0/sessions',function (response) {
+        if(!(response.data.user_id && response.data.name)){
+            location.href = '/login.html'
+        }
+    });
 
     $(".input-daterange").datepicker({
         format: "yyyy-mm-dd",
@@ -42,7 +47,8 @@ $(document).ready(function(){
         } else {
             var sd = new Date(startDate);
             var ed = new Date(endDate);
-            days = (ed - sd)/(1000*3600*24) + 1;
+            // days = (ed - sd)/(1000*3600*24) + 1;
+            days = (ed - sd)/(1000*3600*24);
             var price = $(".house-text>p>span").html();
             var amount = days * parseFloat(price);
             $(".order-amount>span").html(amount.toFixed(2) + "(共"+ days +"晚)");
@@ -52,6 +58,16 @@ $(document).ready(function(){
     var houseId = queryData["hid"];
 
     // TODO: 获取房屋的基本信息
+    $.get('/api/1.0/houses/detail/'+houseId,function (response) {
+        if (response.errno =='0'){
+            //渲染界面
+            $('.house-info>img').attr('src',response.data.house.img_urls[0]);
+            $('.house-text>h3').html(response.data.house.title);
+            $('.house-text span').html((response.data.house.price/100).toFixed(2));
 
+        }else{
+            alert(response.errmsg);
+        }
+    });
     // TODO: 订单提交
-})
+});
