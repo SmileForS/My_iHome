38c93d47e51e608766eedfd9346db3a5c3011c7f
LL <cpyfluoxiaohui@163.com>
2018-04-09 17:04:50 +0800

    我的订单后端逻辑

--- iHome_LL/api_1_0/order.py
+++ iHome_LL/api_1_0/order.py
@@ -7,6 +7,39 @@ import datetime
 from iHome_LL.models import House,Order
 from iHome_LL import db
 
+
+@api.route('/orders')
+@login_required
+def get_order_list():
+    """
+    获取订单列表
+    0.判断用户是否登录
+    1.获取参数,user_id = g.user_id
+    2.查询用户user_id对应的订单
+    3.构造响应数据
+    4.响应结果
+    :return:
+    """
+    # 1.获取参数,user_id = g.user_id
+    user_id = g.user_id
+    # 2.查询用户user_id对应的订单
+    try:
+        orders = Order.query.filter(Order.user_id==user_id).all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DATAERR,errmsg=u'查询订单信息失败')
+    if not orders:
+        return jsonify(errno=RET.NODATA,errmsg=u'订单不存在')
+    # 3.构造响应数据
+    orders_dict_list = []
+    for order in orders:
+        orders_dict_list.append(order.to_dict())
+
+    # 4.响应结果
+    return jsonify(errno=RET.OK,errmsg=u'OK',data = orders_dict_list)
+
+
+
 @api.route('/orders',methods=['POST'])
 @login_required
 def create_order():
@@ -30,10 +63,15 @@ def create_order():
     if not all([house_id,start_date_str,end_date_str]):
         return jsonify(errno=RET.PARAMERR,errmsg=u'缺少参数')
     # 校验入住时间是否小于离开时间
-
-    start_date = datetime.datetime.strptime(start_date_str,'%Y-%m-%d')
-    end_date = datetime.datetime.strptime(end_date_str, '%Y-%m-%d')
-    if not end_date > start_date:
+    try:
+        start_date = datetime.datetime.strptime(start_date_str,'%Y-%m-%d')
+        end_date = datetime.datetime.strptime(end_date_str, '%Y-%m-%d')
+        # if not end_date > start_date:
+        #     return jsonify(errno=RET.PARAMERR,errmsg=u'入住时间有误')
+        if end_date and start_date:
+            assert end_date>start_date,Exception(u'入住时间有误')
+    except Exception as e:
+        current_app.logger.error(e)
         return jsonify(errno=RET.PARAMERR,errmsg=u'入住时间有误')
     #　查询数据库，看house_id对应的房屋是否存在
     try:
